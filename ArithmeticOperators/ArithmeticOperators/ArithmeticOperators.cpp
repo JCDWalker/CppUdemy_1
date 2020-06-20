f400cb3d8894cdc1d35bbe9d7983ab07bd43d9eb
JCDWalker <jcdwalker@gmail.com>
2020-06-20 12:52:01 -0400

    Arithmetic operators and reference variables

---
+++ ArithmeticOperators/ArithmeticOperators/ArithmeticOperators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int first{ 20 };
+    int second{ 10 };
+    int result;
+    
+    result = first + second;
+    cout << "Result: " << result << endl;
+    
+    return 0;
+}
